1a848bffa485d1dcdaa43aed912af842b262d045
Cesar <Cesarsefuedesabatico>
2023-02-28 18:36:44 -0300

    El testing, falta la 7

--- database/script.sql
+++ database/script.sql
@@ -1,1 +1,14 @@
 -- Create here your sql script
+create database biblioteca;
+CREATE TABLE autor(id int, nombre varchar(255));
+INSERT INTO autor (id, nombre) VALUES (1, "Stephen King");
+INSERT INTO autor (id, nombre) VALUES (2, "Jorge Luis Borges");
+INSERT INTO autor (id, nombre) VALUES (3, "J.R.R Tolkien");
+INSERT INTO autor (id, nombre) VALUES (5, "Julio Cortázar");
+CREATE TABLE libro(id int, titulo varchar(255), autor_id int);
+INSERT INTO libro (id, titulo, autor_id) VALUES (1, "El aleph", 1);
+INSERT INTO libro (id, titulo, autor_id) VALUES (2, "Rayuela", 5);
+INSERT INTO libro (id, titulo, autor_id) VALUES (3, "Bestiario", 5);
+
+
+
